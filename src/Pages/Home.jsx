5081adaf53d51172dd6c1f2094e0f1378e7cd63f
rootfinchreesePOI5 <mohamedabdiaziz515@gmail.com>
2024-12-29 12:23:34 +0300

    second

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '../Components/Hero'
-import SpecialityMenu from '../Components/specialityMenu'
-import { specialityData } from '../assets/assets_frontend/assets'
+import SpecialityMenu from '../Components/SpecialityMenu'
 import TopDoctors from '../Components/TopDoctors'
 import Banner from '../Components/Banner'
 
